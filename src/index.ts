0f605b0f7aa7834742176ff8dee4caff3ea555d7
ArekRGW <arkadiusz.pawlak.web@gmail.com>
2020-06-23 15:34:00 +0200

    Installing packages and done some basic setup

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import dotenv from "dotenv";
 import express from "express";
+import bodyParser from "body-parser";
 dotenv.config();
+
 const port = process.env.PORT || 3000;
 const app = express();
+
+app.use(bodyParser.json());
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
